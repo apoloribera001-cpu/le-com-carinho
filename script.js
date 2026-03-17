0fee4522d7ba77ce16da64ff6256adbe117482ef
apoloribera001-cpu <apoloribera001@gmail.com>
2026-03-17 19:49:09 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const card = document.getElementById('card');
+const openBtn = document.getElementById('open');
+const closeBtn = document.getElementById('close');
+
+openBtn.addEventListener('click', () => {
+  card.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+  card.classList.remove('open');
+});
+
+/* PÁGINAS */
+let currentPage = 0;
+const pages = document.querySelectorAll('.page');
+const nextBtn = document.getElementById('nextPage');
+
+nextBtn.addEventListener('click', () => {
+  pages[currentPage].classList.remove('active');
+  currentPage++;
+
+  if (currentPage >= pages.length) {
+    currentPage = 0;
+  }
+
+  pages[currentPage].classList.add('active');
+});
